d0d439219acae3a5a9bc0ac083a113687c266894
Gurmehar <thedovesscar@gmail.com>
2019-08-10 00:03:45 -0700

    Added lazily loaded structure to SingletonExample class

--- src/org/dove/pattern/singleton/SingletonExample.java
+++ src/org/dove/pattern/singleton/SingletonExample.java
@@ -2,13 +2,18 @@ package org.dove.pattern.singleton;
 
 public class SingletonExample {
 
-	private static SingletonExample instance = new SingletonExample();
+	private static SingletonExample instance = null;
 	
 	private SingletonExample() {
 		
 	}
 	
 	public SingletonExample getInstance() {
+		
+		if (instance == null) {
+			instance = new SingletonExample();
+		}
+		
 		return instance;
 	}
 }

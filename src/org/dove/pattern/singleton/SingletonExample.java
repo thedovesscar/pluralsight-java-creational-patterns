20f4506c13abe025fb2f140698518a55a9834ef7
Gurmehar <thedovesscar@gmail.com>
2019-08-10 15:21:55 -0700

    Added code to make Singleton class threadsafe

--- src/org/dove/pattern/singleton/SingletonExample.java
+++ src/org/dove/pattern/singleton/SingletonExample.java
@@ -2,18 +2,23 @@ package org.dove.pattern.singleton;
 
 public class SingletonExample {
 
-	private static SingletonExample instance = null;
+	private static volatile SingletonExample instance = null;
 	
 	private SingletonExample() {
-		
+		if (instance != null) {
+			throw new RuntimeException("Use getInstance() method "
+					+ " to create SingletonExample instance");
+		}
 	}
 	
 	public SingletonExample getInstance() {
-		
 		if (instance == null) {
-			instance = new SingletonExample();
+			synchronized (SingletonExample.class) {
+				if (instance == null) {
+					instance = new SingletonExample();
+				}
+			}
 		}
-		
 		return instance;
 	}
 }

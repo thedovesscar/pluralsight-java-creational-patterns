85386a7e663a1f258733b9fb13508f66a92355f2
Gurmehar <thedovesscar@gmail.com>
2019-08-10 00:00:46 -0700

    Added basic source code to SingletonExample class

--- src/org/dove/pattern/singleton/SingletonExample.java
+++ src/org/dove/pattern/singleton/SingletonExample.java
@@ -2,5 +2,13 @@ package org.dove.pattern.singleton;
 
 public class SingletonExample {
 
+	private static SingletonExample instance = new SingletonExample();
 	
+	private SingletonExample() {
+		
+	}
+	
+	public SingletonExample getInstance() {
+		return instance;
+	}
 }
